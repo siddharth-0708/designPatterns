2cbce480c215dceb3a53c6a43f6fedf74f2594f0
siddharth-0708 <siddharth0708@gmail.com>
2025-03-25 08:16:22 +0530

    solid principles

--- SOLID/dependencyInversionPrinciple.js
+++ SOLID/dependencyInversionPrinciple.js
@@ -1,4 +1,5 @@
 /*
 Dependency Inversion Principle:
 Dependency Injection (DI) is a design pattern where a class receives its dependencies from an external source rather than creating them inside the class itself. This helps achieve loose coupling, making the class more modular, flexible, and testable.
+CHECK CHAT GPT AND UDEMY EXAMPLES
 */
