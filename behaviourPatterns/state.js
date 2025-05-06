fa5415f76fb068e4bcbdae4b2ad3020c1610cd52
siddharth-0708 <siddharth0708@gmail.com>
2025-05-06 22:53:40 +0530

    state design pattern

--- behaviourPatterns/state.js
+++ behaviourPatterns/state.js
@@ -39,3 +39,107 @@ class Dummy1 extends DummyAbs{
 // console.log(b.name);
 
 
+/* STATE DESIGN PATTERN:
+
+The State Design Pattern is a behavioral design pattern that lets an object alter its behavior when its internal state changes. It appears as if the object changed its class.
+
+Instead of using multiple if/else or switch statements to handle behavior based on the current state, the object delegates behavior to different state objects.
+
+Each state object encapsulates the behavior associated with a particular state.
+
+🧠 Core Concept:
+Think of it like a finite state machine. An object can be in one state at a time, and based on events, it transitions to other states, changing its behavior accordingly.
+
+*/
+
+//option1
+class FanOn {
+    message(){
+        console.log("fan is switched on");
+        
+    }
+}
+
+class FanOff {
+    message(){
+        console.log("fan is switched off");
+    }
+}
+
+class Fan {
+    constructor(){
+        this.fanOn = new FanOn();
+        this.fanOff = new FanOff();
+        this.state = this.fanOff;
+        this.state.message();
+    }
+    on(){
+        this.state = this.fanOn;
+        this.state.message();
+    }
+    off(){
+        this.state = this.fanOff;
+        this.state.message();
+    }
+}
+// const fan = new Fan();
+
+// fan.on();
+// fan.off();
+
+//option2 - 3 states
+
+class LowSpeed {
+    constructor(context){
+        this.context = context;
+    }
+    toggle(){
+        console.log("Fan is in low speed now");
+        this.context.state = this.context.highSpeed;
+        
+    }
+}
+
+class HighSpeed {
+    constructor(context){
+        this.context = context;
+    }
+    toggle(){
+        console.log("Fan is in high speed now");
+        this.context.state = this.context.off;
+        
+    }
+}
+
+class Off {
+    constructor(context){
+        this.context = context;
+    }
+    toggle(){
+        console.log("Fan is switched off now");        
+        this.context.state = this.context.lowSpeed;
+        
+    }
+}
+
+class Fan1 {
+    constructor(){
+        this.lowSpeed = new LowSpeed(this);
+        this.highSpeed = new HighSpeed(this);
+        this.off = new Off(this);
+        this.state = this.off;
+        this.state.toggle();
+    }
+    press(){
+        this.state.toggle();
+    }
+}
+
+const b = new Fan1();
+b.press();
+b.press();
+b.press();
+b.press();
+b.press();
+b.press();
+b.press();
